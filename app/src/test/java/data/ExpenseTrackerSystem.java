e68053a02ef375b53bc9cab67bf33b0c45689298
kenneth <kfhv.24@gmail.com>
2019-04-07 15:18:31 -0600

    Added total attribute for ExpenseTrackerSystem.

--- app/src/test/java/data/ExpenseTrackerSystem.java
+++ app/src/test/java/data/ExpenseTrackerSystem.java
@@ -6,17 +6,43 @@ public class ExpenseTrackerSystem {
     ArrayList<Register> registersList;
     private double totalExpended;
     private double totalIncomes;
+    private double total;
+
     public ExpenseTrackerSystem(){
         registersList = new ArrayList<Register>() {};
 
         totalExpended = 0;
         totalIncomes = 0;
+        total = 0;
     }
 
-    public void addIncome(){
-
+    public ArrayList<Register> getRegistersList(){
+        return registersList;
     }
 
+    public void addIncome(float value, String description){
+        registersList.add(new Income(value, description));
+        totalIncomes += value;
+        total += value;
+    }
 
+    public void addExpense(float value, Expense.Category category, String description){
+        registersList.add(new Expense(value, category, description));
+        totalExpended += value;
+        total -= value;
+    }
+
+    public void updateValues(){
+        totalExpended = 0;
+        totalIncomes = 0;
+        for (Register register: registersList) {
+            if(register instanceof Income){
+                totalIncomes += register.getValue();
+            } else {
+                totalExpended += register.getValue();
+            }
+        }
+        total = totalIncomes - totalExpended;
+    }
 
 }
