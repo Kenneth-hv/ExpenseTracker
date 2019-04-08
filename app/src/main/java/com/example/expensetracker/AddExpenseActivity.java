d95a6b4d2abdb85331d30972ebeb10c43aa1f492
kenneth <kfhv.24@gmail.com>
2019-04-07 22:14:07 -0600

    Now is possible to add Expenses.

--- app/src/main/java/com/example/expensetracker/AddExpenseActivity.java
+++ app/src/main/java/com/example/expensetracker/AddExpenseActivity.java
@@ -2,6 +2,13 @@ package com.example.expensetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.expensetracker.controler.ExpenseTracker;
+import com.example.expensetracker.data.Expense;
+
 
 public class AddExpenseActivity extends AppCompatActivity {
 
@@ -9,5 +16,15 @@ public class AddExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+    }
+
+    public void addExpenseButtonClick(View view){
+
+        EditText editText_expense = findViewById(R.id.expenseInput);
+
+
+        float value = Float.parseFloat(editText_expense.getText().toString());
+        ExpenseTracker.getInstance().addExpense(value, "Gasto", Expense.Category.OTHER);
+
     }
 }
