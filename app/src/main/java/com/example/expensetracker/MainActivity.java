072a0d56f5eb655f25779d6ae8806c93a6c843b8
Eduard Torres <eduardcrsk8@users.noreply.github.com>
2019-04-08 01:07:21 -0600

    Guarda la instancia de ExpenseTracker en un JSON

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.expensetracker;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.expensetracker.controler.ExpenseTracker;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         expenseTracker = ExpenseTracker.getInstance();
+        System.out.print(getApplicationInfo().dataDir);
+        loadJSON();
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveJSON();
     }
 
     public void addExpenseButton(View view){
@@ -29,6 +42,27 @@ public class MainActivity extends AppCompatActivity {
     public void addIncomeButton(View view){
         Intent addIncomeIntent = new Intent(this, AddIncomeActivity.class);
         startActivity(addIncomeIntent);
+
     }
 
+    public void saveJSON() {
+        SharedPreferences sharedPreferences = getSharedPreferences("tracker", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(expenseTracker);
+        editor.putString("registers", json);
+        editor.apply();
+    }
+
+    public void loadJSON() {
+        SharedPreferences sharedPreferences = getSharedPreferences("tracker", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("registers", null);
+        Type type = new TypeToken<ExpenseTracker>() {
+        }.getType();
+        expenseTracker = gson.fromJson(json, type);
+
+        if (expenseTracker == null)
+            expenseTracker = ExpenseTracker.getInstance();
+    }
 }
